0f5b130e2ab0acd22ffe0808e515541a0ff4b22e
Shawon Ghosh <shawon.ghosh@bigbasket.com>
2025-05-13 10:25:09 +0530

    App.css fixed

--- frontend/src/components/TemplatesSidebar.jsx
+++ frontend/src/components/TemplatesSidebar.jsx
@@ -219,8 +219,8 @@ export const TemplatesSidebar = ({
         <Typography variant="h6" className="section-heading">Process Templates</Typography>
 
         {/* Process Template Search */}
-        <div className="search">
-          <div className="searchInputs">
+        <div className="newsearch">
+          <div className="newsearchInputs">
             <input
               ref={processTemplateRef}
               type="text"
@@ -238,7 +238,7 @@ export const TemplatesSidebar = ({
               <path d="M 21 3 C 11.621094 3 4 10.621094 4 20 C 4 29.378906 11.621094 37 21 37 C 24.710938 37 28.140625 35.804688 30.9375 33.78125 L 44.09375 46.90625 L 46.90625 44.09375 L 33.90625 31.0625 C 36.460938 28.085938 38 24.222656 38 20 C 38 10.621094 30.378906 3 21 3 Z M 21 5 C 29.296875 5 36 11.703125 36 20 C 36 28.296875 29.296875 35 21 35 C 12.703125 35 6 28.296875 6 20 C 6 11.703125 12.703125 5 21 5 Z" />
             </svg>
             <div
-              className="searchData"
+              className="newsearchData"
               style={{ visibility: processTemplateListDisplay }}
             >
               {filteredProcessTemplates.map((template) => {
@@ -266,8 +266,8 @@ export const TemplatesSidebar = ({
         <Typography variant="h6" className="section-heading">Master Task Templates</Typography>
 
         {/* Task Template Search */}
-        <div className="search">
-          <div className="searchInputs">
+        <div className="newsearch">
+          <div className="newsearchInputs">
             <input
               ref={taskTemplateRef}
               type="text"
@@ -285,7 +285,7 @@ export const TemplatesSidebar = ({
               <path d="M 21 3 C 11.621094 3 4 10.621094 4 20 C 4 29.378906 11.621094 37 21 37 C 24.710938 37 28.140625 35.804688 30.9375 33.78125 L 44.09375 46.90625 L 46.90625 44.09375 L 33.90625 31.0625 C 36.460938 28.085938 38 24.222656 38 20 C 38 10.621094 30.378906 3 21 3 Z M 21 5 C 29.296875 5 36 11.703125 36 20 C 36 28.296875 29.296875 35 21 35 C 12.703125 35 6 28.296875 6 20 C 6 11.703125 12.703125 5 21 5 Z" />
             </svg>
             <div
-              className="searchData"
+              className="newsearchData"
               style={{ visibility: taskTemplateListDisplay }}
             >
               {filteredMasterTemplates.map((task) => (
@@ -353,8 +353,8 @@ export const TemplatesSidebar = ({
                   </Typography>
 
                   {/* Child Task Search */}
-                  <div className="search">
-                    <div className="searchInputs">
+                  <div className="newsearch">
+                    <div className="newsearchInputs">
                       <input
                         ref={childTaskSearchRef}
                         type="text"
